d37654ba0c1200378af69537af93ffb18c3734b2
clouless <github@gruenewaldt.net>
2018-01-03 11:34:20 +0100

    v1.1.1 export theme

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from './components/notification.module';
 export * from './components/notification.model';
+export * from './components/notification.theme';
 export * from './components/services/notification.service';
